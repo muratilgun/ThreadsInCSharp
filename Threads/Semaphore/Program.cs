6e84736fc73eb99718164284d3e77a61c6486c9d
muratilgun <muratilgun34@gmail.com>
2022-09-29 21:38:43 +0300

    Semaphore

---
+++ Threads/Semaphore/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Semaphore
+{
+    internal class Program
+    {
+        private static System.Threading.Semaphore _semaphore = new System.Threading.Semaphore(1,1);
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Write).Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void Write()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Waiting...");
+            _semaphore.WaitOne();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Writing...");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Writing Completed...");
+            _semaphore.Release();
+        }
+    }
+
+}
