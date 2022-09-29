e6af9fd1d53d7d3a7f91f33b34b6a0acf7535c12
muratilgun <muratilgun34@gmail.com>
2022-09-29 19:46:36 +0300

    Created Lock Project

---
+++ Threads/Lock/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(DoWork).Start();
+            }
+
+            Console.ReadKey();
+        }
+
+
+        public static void DoWork()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starting..");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} completed..");
+
+        }
+    }
+}
