e95234374b670ab1ebcae2c3947cf237e803234d
muratilgun <muratilgun34@gmail.com>
2022-09-29 21:12:57 +0300

    AutoResetEvent

---
+++ Threads/AutoResetEvent/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AutoResetEvent
+{
+    internal class Program
+    {
+        private static System.Threading.AutoResetEvent _are = new System.Threading.AutoResetEvent(true);
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Write).Start();
+            }
+
+            Thread.Sleep(3000);
+            //_are.Set();
+            Console.ReadKey();
+        }
+
+        public static void Write()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Waiting...");
+            _are.WaitOne();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Writing...");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Writing Completed...");
+            _are.Set();
+        }
+    }
+}
