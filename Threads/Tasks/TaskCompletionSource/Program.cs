b956dea6160a8d25ebe155ccbb662d5b921a2c49
muratilgun <muratilgun34@gmail.com>
2022-10-01 23:29:13 +0300

    TaskCompletionSource

---
+++ Threads/Tasks/TaskCompletionSource/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskCompletionSource
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+            var tsc = new TaskCompletionSource<bool>();
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                tsc.TrySetResult(true);
+
+            }).Start();
+
+            var a = tsc.Task.Result;
+
+        }
+    }
+}
