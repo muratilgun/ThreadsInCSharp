1e3d642e08e84ec1e3e47acbbde3e1d1f10e8fa1
muratilgun <muratilgun34@gmail.com>
2022-10-02 21:21:36 +0300

    Polling Wait

---
+++ Threads/PollingWait/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PollingWait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before polling thread");
+            var pollComplete = false;
+
+            var pollingThread = new Thread(() =>
+            {
+                Console.WriteLine("Inside polling thread");
+                Thread.Sleep(500);
+                //Set complete
+                pollComplete = true;    
+            });
+
+            pollingThread.Start();
+
+            while (!pollComplete)
+            {
+                Console.WriteLine("Polling...");
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("After polling thread");
+            Console.ReadLine();
+        }
+    }
+}
