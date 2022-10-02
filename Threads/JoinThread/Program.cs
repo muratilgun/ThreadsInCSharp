dc87ba50f7d6ee322a8efbddc7f2b3952a139125
muratilgun <muratilgun34@gmail.com>
2022-10-02 21:12:40 +0300

    Thread Join

---
+++ Threads/JoinThread/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace JoinThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("1. #####");
+            }).Start();
+            Console.WriteLine("2. #####");
+
+            var blockingThread = new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("3. #####");
+
+            });
+
+            blockingThread.Start();
+            blockingThread.Join();
+            Console.WriteLine("4. #####");
+
+            // Console.ReadLine();
+
+        }
+    }
+}
