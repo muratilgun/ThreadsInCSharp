ba3615085200f730f8fa12949e81e798c447586f
muratilgun <muratilgun34@gmail.com>
2022-10-02 21:40:06 +0300

    EventBasedWait

---
+++ Threads/EventbasedWait/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace EventbasedWait
+{
+    internal class Program
+    {
+        private static event Action EventFinished = () => { };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before event thread");
+
+            var eventThread = new Thread(() =>
+            {
+                Console.WriteLine("Inside event thread");
+                Thread.Sleep(500);
+
+                // Fire completed event
+                EventFinished();
+            });
+
+            // Hook into callback event
+
+            EventFinished += () =>
+            {
+                //Called back from thread
+                Console.WriteLine("Event thread callback on complete");
+            };
+
+
+            eventThread.Start();
+
+
+            Console.WriteLine("After event thread");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("***********************************************");
+
+
+            Console.WriteLine("Before event method thread");
+
+            EventThreadCallbackMethod(() =>
+            {
+                //Called back from thread
+                Console.WriteLine("Event thread callback on complete");
+            });
+
+            Console.WriteLine("After event method thread");
+            Thread.Sleep(1000);
+            Console.WriteLine("***********************************************");
+
+
+            Console.ReadLine();
+        }
+
+        private static void EventThreadCallbackMethod(Action completed)
+        {
+            new Thread(() =>
+            {
+                Console.WriteLine("Inside event thread method");
+                Thread.Sleep(500);
+                completed();
+            }).Start();
+
+        }
+    }
+}
